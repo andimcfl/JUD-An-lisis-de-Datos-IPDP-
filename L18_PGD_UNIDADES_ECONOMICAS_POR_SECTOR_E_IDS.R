d9e7391edca8fce94a1de045702397bfad4274fa
andimcfl <andi.mancera@gmail.com>
2025-11-10 14:39:42 -0600

    zfsd

--- L18_PGD_UNIDADES_ECONOMICAS_POR_SECTOR_E_IDS.R
+++ L18_PGD_UNIDADES_ECONOMICAS_POR_SECTOR_E_IDS.R
@@ -1,51 +1,79 @@
-#Limpiar la zona de trabajo
+#********************************************************************************
+#    INSTITUTO DE PLANEACIÓN DEMOCRÁTICA Y PROSPECTIVA DE LA CIUDAD DE MÉXICO   *
+#********************************************************************************
+#********************************************************************************             
+#*                                                                              * 
+#*    Metodología para el mapa de área de distribución de área de               *
+#*    espacio público cubierta por árboles y arbustos por habitante,            *
+#*    por alcaldía.                                                             *
+#*    Línea XVIII. De Economía Dinámica, Innovadora y de Prosperidad            * 
+#*                 Compartida                                                   *
+#*    Plan General de Desarrollo de la Ciuad de México                          *
+#*                                                                              * 
+#********************************************************************************
+#Limpiar la zona de trabajo----
 rm(list=ls())
-#Librerias
-if (require('pacman')) install.packages('pacman')
-pacman::p_load(tidyverse,dplyr,sf,classInt)
+#Librerias----
+if (!require('pacman')) install.packages('pacman')
+pacman::p_load(tidyverse,dplyr,sf,classInt, googledrive, tmap, paletteer)
+#Crear directorio----
+dir.create("BASE")
+directorio_base <- "BASE"
+setwd(directorio_base)
+getwd()
 
-hexagonos<- st_read("C:/Users/brenp/Desktop/CARTOGRAFIA PGD/H3_8_CDMX/H3_8_CDMX.shp")|>
+#Cargar la capa de exágonos para hacer el agrupado de los datos----
+jp_file <- as_id("https://drive.google.com/file/d/1btYonN0jWimM19WFMmloh853Asb1jihu/view?usp=sharing")
+drive_download(jp_file, overwrite = TRUE)
+zip::unzip('H3_8_CDMX.zip',  exdir = "H3_8_CDMX")
+
+hexagonos<- st_read("H3_8_CDMX/H3_8_CDMX.shp")|>
   st_transform(32614)|>
+  select(h3_id)|>
   mutate(area = as.numeric(st_area(geometry)) / 10000)
 
-##AGREGAR EL IDS POR MANZANA
-ids_mza <- st_read("C:/Users/brenp/Desktop/CARTOGRAFIA PGD/linea economia/mza_shp_ids_evalua/ids_mza_cdmx.shp")|>
+##IDS por manzana----
+url<- "https://www.evalua.cdmx.gob.mx/storage/app/media/2024/die/ids/mza_shp.zip"
+options(timeout = 5000)
+download.file(url, 'mza_shp.zip', mode = "wb")
+zip::unzip("mza_shp.zip")
+
+ids_mza <- st_read("mza_shp/ids_mza_cdmx.shp")|>
   st_transform(32614)|>
   select(ids)|>
-  st_centroid()
-
-ids_mza_j<-ids_mza|>
+  st_centroid()|>
   st_intersection(hexagonos)|>
   st_drop_geometry()|>
-  as.data.frame()
-
-ids_mza_j_datos <- ids_mza_j|>
+  as.data.frame()|>
   group_by(h3_id)|>
   summarise(ids_p = mean(ids, na.rm = TRUE))
 
 
-##DENUE
-denue_25 <- st_read("C:/Users/brenp/Desktop/CARTOGRAFIA PGD/linea economia/2025")|>
+##Cargar unidades económicas y seleccionar las de los sectores servicios, comercio e industria----
+url<- "https://www.inegi.org.mx/contenidos/masiva/denue/denue_09_shp.zip"
+download.file(url, 'denue_09_shp.zip', mode = "wb")
+zip::unzip("denue_09_shp.zip", exdir = "denue_09_shp")
+
+##Agregar las unidades económicas a la malla hexagonal para obtener la densidad de unidades por sector en cada hexágono----
+denue_25<- st_read("denue_09_shp/denue_09.shp")|>
   st_transform(32614)|>
-  select(actividad)
-
-denue_25s<-denue_25|>
-  st_intersection(hexagonos)|>
-  st_drop_geometry()|>
-  as.data.frame()
-
-
-denue_25s_ <- denue_25s|>
+  mutate(sector = substr(codigo_act, 1,2), 
+         actividad = )|>
+  filter(actividad %in% c())
+  select(actividad) |>
+    st_intersection(hexagonos)|>
+    st_drop_geometry()|>
+    as.data.frame()|>
   group_by(h3_id,actividad)|>
-  summarise(u = n(), .groups = "drop") |>
+  summarise(u = n(), .groups = "drop")|>
   pivot_wider(names_from = actividad,
               values_from = u,
               values_fill = 0)|>
   select(h3_id,Comercio, Industria, Servicios)
 
-
+#Obtener la sendidad de unidades por cada sector----
 hex_class <- hexagonos|>
-  left_join(ids_mza_j_datos, by = "h3_id")|>
+  left_join(ids_mza, by = "h3_id")|>
   mutate(ids_p = case_when(is.na(ids_p) ~ 0,
                            TRUE ~ ids_p))|>
   left_join(denue_25s_, by = "h3_id")|>
@@ -60,7 +88,7 @@ hex_class <- hexagonos|>
                                 TRUE ~ dens_serv))
   
   
-
+#Función para la correlación bivariada entre el IDS y la densidad de unidades económicas----
 correlacion_bivariada <- function(df, var1, var2, nombre) {
   
   # Filtrar solo donde ambos valores sean > 0
@@ -74,7 +102,7 @@ correlacion_bivariada <- function(df, var1, var2, nombre) {
   clase1 <- rep(NA_integer_, nrow(df))
   
   if (var1 == "ids_p") {
-    # Clasificación personalizada con rangos fijos
+    # Clasificación personalizada con rangos fijos con base en la metodología de EVALÚA
     clase1[filtro_valido] <- cut(
       df[[var1]][filtro_valido],
       breaks = c(-Inf, 0.784, 0.860, 1),
@@ -119,19 +147,36 @@ correlacion_bivariada <- function(df, var1, var2, nombre) {
   return(resultado)
 }
 
-# Aplicar la función a tus pares de variables
+# Aplicar la función por cada par de variables----
 biv_com  <- correlacion_bivariada(hex_class, "ids_p", "dens_com",   "com")
 biv_ind  <- correlacion_bivariada(hex_class, "ids_p", "dens_indus", "indus")
 biv_ser  <- correlacion_bivariada(hex_class, "ids_p", "dens_serv",  "serv")
 
-# Unir resultados a la capa original
+# Unir resultados a la capa original----
 hex_class <- cbind(hex_class, biv_com, biv_ind, biv_ser)|>
-  select(h3_id,u_servicio,area,ids_p,Comercio,Industria,Servicios,
-         dens_com,dens_indus,dens_serv,com_ids_p_nivel,com_dens_com_nivel,
+  select(h3_id, com_ids_p_nivel,com_dens_com_nivel,
          indus_ids_p_nivel,indus_dens_indus_nivel,serv_ids_p_nivel,serv_dens_serv_nivel,
          clase_com_txt,clase_indus_txt,clase_serv_txt)
 
-# Guardar como shapefile
-st_write(hex_class, "C:/Users/brenp/Desktop/CARTOGRAFIA PGD/ids_ueconomicas_classes_completas.gpkg", delete_layer = TRUE)
+# Guardar----
+st_write(hex_class, "ids_ueconomicas_classes_completas.gpkg", delete_layer = TRUE)
+
+
+#Mapear----
+
+tmap_mode("view")
+tm_basemap("OpenStreetMap") +
+  
+  tm_shape(hex_class) +
+  tm_polygons("clase_com_txt",
+              style = "fixed",
+              breaks = breaks_precio,
+              palette = paletteer_c("ggthemes::Orange", 5), 
+              alpha = 0.8,
+              title = "Precio por m² de vivienda (mediana)") +
+  
+  tm_layout(legend.position = c("left", "bottom"))
+
+
 
 
